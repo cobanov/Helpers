71b8f3448e6c9a2586ad849343880e7dc78c6657
FIXED-TERM Cobanoglu Ahmet Mert (BuP1/MSI-GA) <fixed-term.Mert.Cobanoglu@tr.bosch.com>
2019-05-14 09:46:22 +0300

    duzenle

---
+++ deeplearning/gpu_available.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+
+tf.enable_eager_execution()
+
+print(tf.add(1, 2))
+print(tf.add([1, 2], [3, 4]))
+print(tf.square(5))
+print(tf.reduce_sum([1, 2, 3]))
+print(tf.encode_base64("hello world"))
+
+# Operator overloading is also supported
+print(tf.square(2) + tf.square(3))
+
+x = tf.matmul([[1]], [[2, 3]])
+print(x.shape)
+print(x.dtype)
+
+import numpy as np
+
+ndarray = np.ones([3, 3])
+
+print("TensorFlow operations convert numpy arrays to Tensors automatically")
+tensor = tf.multiply(ndarray, 42)
+print(tensor)
+
+
+print("And NumPy operations convert Tensors to numpy arrays automatically")
+print(np.add(tensor, 1))
+
+print("The .numpy() method explicitly converts a Tensor to a numpy array")
+print(tensor.numpy())
+
+x = tf.random_uniform([3, 3])
+
+print("Is there a GPU available: "),
+print(tf.test.is_gpu_available())
+
+print("Is the Tensor on GPU #0:  "),
+print(x.device.endswith('GPU:0'))
+
+import time
+
+def time_matmul(x):
+  start = time.time()
+  for loop in range(10):
+    tf.matmul(x, x)
+
+  result = time.time()-start
+    
+  print("10 loops: {:0.2f}ms".format(1000*result))
+
+
+# Force execution on CPU
+print("On CPU:")
+with tf.device("CPU:0"):
+  x = tf.random_uniform([1000, 1000])
+  assert x.device.endswith("CPU:0")
+  time_matmul(x)
+
+# Force execution on GPU #0 if available
+if tf.test.is_gpu_available():
+  with tf.device("GPU:0"): # Or GPU:1 for the 2nd GPU, GPU:2 for the 3rd etc.
+    x = tf.random_uniform([1000, 1000])
+    assert x.device.endswith("GPU:0")
+    time_matmul(x)
