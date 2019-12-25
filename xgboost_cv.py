92ae3e7ce75797e16e7583e954009be3219bb1b7
Cobanoglu Ahmet Mert (BuP1/MSI-GA) <Mert.Cobanoglu@tr.bosch.com>
2019-12-25 12:59:48 +0300

    xgboost cv

---
+++ xgboost_cv.py
@@ -0,0 +1,12 @@
+import xgboost as xgboost
+import pandas as pd
+
+churn_data = pd.read_csv("classification_data.csv")
+
+churn_dmatrix = xgb.DMatrix(data=churn_data.iloc[:, -1],
+                            label=churn_data.month_5_still_here)
+
+params = {"objective":"binary:logistic", max_depth=4}
+
+cv_results = xgb.cv(dtrain=churn_dmatrix, params=params, nfold=4,
+                    num_boost_round=10, metrics="error", as_pandas=True)
