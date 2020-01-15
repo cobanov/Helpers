f8ff1fbde39fe093bcdad384b2d73063d8d008a7
Cobanoglu Ahmet Mert (BuP1/MSI-GA) <Mert.Cobanoglu@tr.bosch.com>
2020-01-15 13:23:38 +0300

    pep8

--- contacts/contacts.py
+++ contacts/contacts.py
@@ -1,60 +1,56 @@
-import pandas as pd 
+import pandas as pd
 import glob
 import os
 import sqlite3
 
 
-
 def read_files(file_name):
 
-	with open(file_name) as f:
-		content = f.readlines()
+    with open(file_name) as f:
+        content = f.readlines()
 
-	content = [x.strip() for x in content]
+    content = [x.strip() for x in content]
 
-	return content
+    return content
 
 
 def get_contacts(content):
-	user_names = []
-	names = []
+    user_names = []
+    names = []
 
-	for i in content:
-		
-		user_name, name = i.split("-->")
-		user_names.append(user_name.strip())
-		names.append(name.strip())
+    for i in content:
 
-	return user_names, names
+        user_name, name = i.split("-->")
+        user_names.append(user_name.strip())
+        names.append(name.strip())
 
+    return user_names, names
 
 
 def write_dataframe(user_names, names, file_name):
-	df_contacts = pd.DataFrame(columns=["User_Names", "Name", "Permissions"])	
-	df_contacts["User_Names"] = user_names
-	df_contacts["Name"] = names
-	df_contacts["Permissions"] = file_name
-	return df_contacts
+    df_contacts = pd.DataFrame(columns=["User_Names", "Name", "Permissions"])
+    df_contacts["User_Names"] = user_names
+    df_contacts["Name"] = names
+    df_contacts["Permissions"] = file_name
+    return df_contacts
+
 
 def create_database():
-	conn = sqlite3.connect('contacts.db')
-	print ("Opened database successfully")
+    conn = sqlite3.connect('contacts.db')
+    print("Opened database successfully")
 
-	conn.execute('CREATE TABLE students (user_name TEXT, name TEXT, file_name TEXT)')
-	print ("Table created successfully")
-	conn.close()
-
+    conn.execute(
+        'CREATE TABLE students (user_name TEXT, name TEXT, file_name TEXT)')
+    print("Table created successfully")
+    conn.close()
 
 
 def main():
-	file_name = "publisher.txt"
-	content = read_files(file_name)
-	user_names, names = get_contacts(content)
-	df_contacts = write_dataframe(user_names, names, file_name)
-	print(df_contacts.head())
-
-
-	
+    file_name = "publisher.txt"
+    content = read_files(file_name)
+    user_names, names = get_contacts(content)
+    df_contacts = write_dataframe(user_names, names, file_name)
+    print(df_contacts.head())
 
 
 main()
