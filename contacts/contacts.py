e9034006f85bedc41676f18347e9e17fd8d78d61
Cobanoglu Ahmet Mert (BuP1/MSI-GA) <Mert.Cobanoglu@tr.bosch.com>
2020-01-15 13:20:10 +0300

    update

--- contacts/contacts.py
+++ contacts/contacts.py
@@ -1,20 +1,17 @@
+import pandas as pd 
 import glob
 import os
 import sqlite3
-import pandas as pd 
 
-# conn = sqlite3.connect('contacts.db')
-# print ("Opened database successfully")
 
-# conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-# print ("Table created successfully")
-# conn.close()
 
 def read_files(file_name):
+
 	with open(file_name) as f:
 		content = f.readlines()
 
 	content = [x.strip() for x in content]
+
 	return content
 
 
@@ -30,25 +27,34 @@ def get_contacts(content):
 
 	return user_names, names
 
-def write_dataframe(user_names, names, file_name):	
-	df_contacts = pd.DataFrame(columns=["User_Names", "Name", "Permissions"])
+
+
+def write_dataframe(user_names, names, file_name):
+	df_contacts = pd.DataFrame(columns=["User_Names", "Name", "Permissions"])	
 	df_contacts["User_Names"] = user_names
 	df_contacts["Name"] = names
 	df_contacts["Permissions"] = file_name
 	return df_contacts
 
+def create_database():
+	conn = sqlite3.connect('contacts.db')
+	print ("Opened database successfully")
+
+	conn.execute('CREATE TABLE students (user_name TEXT, name TEXT, file_name TEXT)')
+	print ("Table created successfully")
+	conn.close()
+
 
 
 def main():
-	files = glob.glob("*.txt")
-	df_contacts = pd.DataFrame(columns=["User_Names", "Name", "Permissions"])
-
-	for file_name in files:
-		content = read_files(file_name)
-		user_names, names = get_contacts(content)
-		write_dataframe(user_names, names, file_name)
-		print(df_contacts.head())
+	file_name = "publisher.txt"
+	content = read_files(file_name)
+	user_names, names = get_contacts(content)
+	df_contacts = write_dataframe(user_names, names, file_name)
+	print(df_contacts.head())
 
 
-if __name__ == "__main__":
-	main()
+	
+
+
+main()
